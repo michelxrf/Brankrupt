90bb9fda361d13df37596eeedda2e9eb1e3a189e
Michel <michelxrf@gmail.com>
2025-03-15 20:54:55 -0300

    Teste inicial com novo VFX de morte

---
+++ Assets/Project/Scripts/LowHealthFXJaime.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LowHealthFXJaime : MonoBehaviour
+{
+    // script para ativar camadas do VFX conforme a sanidade
+    [SerializeField] private float spreadFactor;
+    [SerializeField] private float maxAlpha;
+    [SerializeField] private Image[] VfxPanes;
+    void Update()
+    {
+        float step = GameManager.Instance.maxSanityLevel / VfxPanes.Length;
+
+        for (int i = 0; i < VfxPanes.Length; i++)
+        {
+            var pane = VfxPanes[i];
+
+            float xSpike = step * i;
+
+            float value = -spreadFactor * Mathf.Pow(GameManager.Instance.currentSanityLevel - xSpike, 2f) + maxAlpha;
+
+            pane.color = new Color(1f, 1f, 1f, Mathf.Clamp(value, 0, 1f));
+        }
+
+    }
+}
