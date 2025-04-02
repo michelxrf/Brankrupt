ef96e21a7dd4cb5d41fbdebb38f53dadaaf63aab
Michel <michelxrf@gmail.com>
2025-04-01 22:08:17 -0300

    fix de coisa estranha

--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using DialogueEditor;
-using Unity.Burst.Intrinsics;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
