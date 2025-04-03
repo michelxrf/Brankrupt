be1ed842f7c770c62d2e68cb6f32f22de8ef5201
Michel <michelxrf@gmail.com>
2025-04-03 20:54:01 -0300

    pequenos ajustes nos créditos

--- Assets/Project/Scripts/CutsceneController.cs
+++ Assets/Project/Scripts/CutsceneController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using DialogueEditor;
 using TMPro;
-using TMPro.EditorUtilities;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.Video;
 
